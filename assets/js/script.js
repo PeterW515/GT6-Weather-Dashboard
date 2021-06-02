6ff7eb46007fe646c9584d0d35235d5523881373
Peter Weigman <phweigman@gmail.com>
2021-06-01 22:43:21 -0400

    mocked up main html and css. Need to complete JS functionality.

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,9 +8,29 @@ function searchCity(){
     fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityToSearch}&limit=1&appid=8ff1be0bee70c37c62ab2ef02c004244`)
         .then(response => response.json())
         .then(result => searchWeather(result));
-}
+    }
 
 
 function searchWeather(result){
-    console.log(result[0].lat + '   ' + result[0].lon)
+    if(result.length===0){
+        searchText.value = 'Invalid City';
+        return;
+    }
+    console.log(result[0]);
+    let lat = result[0].lat;
+    let lon = result[0].lon;
+
+    weatherResults(lat,lon);
+
+}
+
+
+function weatherResults(lat, lon){
+    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&units=imperial&appid=8ff1be0bee70c37c62ab2ef02c004244`)
+    .then(response => response.json())
+    .then(result => weatherDisplay(result));
+}
+
+function weatherDisplay(result) {
+    console.log(result.current);
 }
